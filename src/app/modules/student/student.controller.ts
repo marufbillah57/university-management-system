c4879545f9fcd4849b1fff361937290dba5f5782
marubillah57 <marufbillah5157@gmail.com>
2023-12-03 16:57:55 +0600

    avoid try catch repetition code and use catchAsync higher order function in utils

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,84 +1,44 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express'
 import { StudentServices } from './student.service'
 import sendResponse from '../../utils/sendResponse'
 import httpStatus from 'http-status'
+import catchAsync from '../../utils/catchAsync'
 
-const getAllStudents = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const students = await StudentServices.getAllStudentsFromDB()
+const getAllStudents = catchAsync(async (req, res) => {
+  const students = await StudentServices.getAllStudentsFromDB()
 
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Students are retrieved successfully',
-    //   data: students,
-    // })
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Students are retrieved successfully',
-      data: students,
-    })
-  } catch (error) {
-    next(error)
-  }
-}
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Students are retrieved successfully',
+    data: students,
+  })
+})
 
-const getSingleStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const studentId = req.params.studentId
-    const student = await StudentServices.getSingleStudentFromDB(studentId)
+const getSingleStudent = catchAsync(async (req, res) => {
+  const studentId = req.params.studentId
+  const student = await StudentServices.getSingleStudentFromDB(studentId)
 
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Student is retrieved successfully',
-    //   data: student,
-    // })
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is retrieved successfully',
+    data: student,
+  })
+})
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is retrieved successfully',
-      data: student,
-    })
-  } catch (error) {
-    next(error)
-  }
-}
+const deleteStudent = catchAsync(async (req, res) => {
+  const { studentId } = req.params
 
-const deleteStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { studentId } = req.params
+  const result = await StudentServices.deleteStudentFromDB(studentId)
 
-    const result = await StudentServices.deleteStudentFromDB(studentId)
-
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Student is deleted successfully',
-    //   data: result,
-    // })
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is deleted successfully',
-      data: result,
-    })
-  } catch (error) {
-    next(error)
-  }
-}
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is deleted successfully',
+    data: result,
+  })
+})
 
 export const StudentControllers = {
   getAllStudents,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,43 +1,26 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express'
-// import studentValidationSchema from '../student/student.validation'
 import { UserServices } from './user.service'
 import sendResponse from '../../utils/sendResponse'
 import httpStatus from 'http-status'
+import catchAsync from '../../utils/catchAsync'
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { password, student: studentData } = req.body
+const createStudent = catchAsync(async (req, res) => {
+  const { password, student: studentData } = req.body
 
-    // const zodData = studentValidationSchema.parse(studentData)
+  // const zodData = studentValidationSchema.parse(studentData)
 
-    // will call service function to send this data
-    const newStudent = await UserServices.createStudentIntoDB(
-      password,
-      studentData,
-    )
+  // will call service function to send this data
+  const newStudent = await UserServices.createStudentIntoDB(
+    password,
+    studentData,
+  )
 
-    // send response
-    // res.status(201).json({
-    //   success: true,
-    //   message: 'Student created successfully',
-    //   data: newStudent,
-    // })
-
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is created successfully',
-      data: newStudent,
-    })
-  } catch (error) {
-    next(error)
-  }
-}
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is created successfully',
+    data: newStudent,
+  })
+})
 
 export const UserControllers = {
   createStudent,
