083612ed994e4528140647ca7278e5dc9a2e4098
marubillah57 <marufbillah5157@gmail.com>
2023-12-07 19:18:36 +0600

    conver zod error

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,22 +1,65 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response, NextFunction, ErrorRequestHandler } from 'express'
+import { ZodError, ZodIssue } from 'zod'
+import { TErrorSource } from '../interface/error'
+import config from '../config'
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = err.statusCode || 500
-  const message = err.message || 'something went wrong'
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // setting default values
+  let statusCode = err.statusCode || 500
+  let message = err.message || 'something went wrong'
 
+  let errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ]
+
+  const handleZodError = (err: ZodError) => {
+    const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue.path.length - 1],
+        message: issue.message,
+      }
+    })
+    statusCode = 400
+    return {
+      statusCode,
+      message: 'Validation Error',
+      errorSources,
+    }
+  }
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err)
+    statusCode = simplifiedError?.statusCode
+    message = simplifiedError?.message
+    errorSources = simplifiedError?.errorSources
+
+    message = 'zod errorrr'
+  }
+
+  // ultimate return
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    stack: config.node_env === 'development' ? err?.stack : null,
   })
 }
 
 export default globalErrorHandler
+
+// pattern
+/*
+  success
+  message
+  errorSources:[
+    path:'',
+    message:''
+  ]
+  stack
+*/
